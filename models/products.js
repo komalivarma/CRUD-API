3166f4b2756b5dd65acadd32b1b8ef5545cf063a
Komali Pathri <kpathri@corp.miraclesoft.com>
2018-12-18 16:30:45 +0530

    Deleted unnecessary files

--- models/products.js
+++ models/products.js
@@ -2,24 +2,27 @@ var db=require('../dbconnection');
 var prod={
 
     getAllProd:function(callback){
+        console.log("Retrieved Data Successfully");
     return db.query("Select * from products",callback);
     
     },
     getProdById:function(pid,callback){
-    
+        console.log("Retrieved Data Successfully");
         return db.query("select * from products where pid=?",[pid],callback);
     },
 
     addProd:function(prod,callback){
-       console.log("Inside ADD function");
+       console.log("Posted Data Successfully");
        
     return db.query("Insert into products values(?,?,?,?)",[prod.pid,prod.pname,prod.pcost,prod.email],callback);
    
     },
     deleteProd:function(pid,callback){
+        console.log("Deleted Data Successfully");
         return db.query("delete from products where pid=?",[pid],callback);
     },
     updateProd:function(pid,prod,callback){
+        console.log("Data updated Successfully");
         return  db.query("update products set pname=?,pcost=?,email=? where pid=?",[prod.pname,prod.pcost,prod.email,pid],callback);
     }
     
